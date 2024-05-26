c39993b4a019f70c59cde96e6a3d6f0ba353621c
miawithcode <cuttontail@gmail.com>
2024-05-26 16:10:31 +0800

    feat: plus and minus count

--- src/CountButtons.jsx
+++ src/CountButtons.jsx
@@ -1,12 +1,19 @@
 import { MinusIcon, PlusIcon } from '@radix-ui/react-icons';
 
-const CountButtons = () => {
+const CountButtons = ({ setCount }) => {
   return (
     <div className="btn-container">
-      <button className="btn minus-btn">
+      <button
+        onClick={() => setCount((prev) => prev - 1)}
+        className="btn minus-btn"
+      >
         <MinusIcon className="minus-btn-icon" />
       </button>
-      <button className="btn plus-btn">
+
+      <button
+        onClick={() => setCount((prev) => prev + 1)}
+        className="btn plus-btn"
+      >
         <PlusIcon className="plus-btn-icon" />
       </button>
     </div>
